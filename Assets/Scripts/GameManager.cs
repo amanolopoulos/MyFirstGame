0370426795acbd8f25601b5cadbfccc932a031c0
Angelos Manolopoulos <angelosmanolopoulos@gmail.com>
2018-07-05 14:16:55 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public bool GameHasEnded = false;
+    public float RestartDelay = 1f;
+    public GameObject CompleteLevelUI;
+
+	public void EndGame()
+    {
+        if (!GameHasEnded)
+        {
+            GameHasEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", RestartDelay);
+        }
+    }
+
+    public void CompleteLevel()
+    {
+        CompleteLevelUI.SetActive(true);
+    }
+    
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody Rigidbody;
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 500f;
+	
+	void FixedUpdate () {
+        Rigidbody.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            Rigidbody.AddForce(SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Rigidbody.AddForce(-SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(Rigidbody.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
